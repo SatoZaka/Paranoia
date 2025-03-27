46dfb7373ed9428f5f4f397faa589214d0d292c0
Sato <lucasgeraghty1@gmail.com>
2025-03-27 00:00:37 -0300

    Added peer connections
    Added peer.js for Peer2Peer connections
    Added connections and data interactions between host and clients

--- Client/sketch.js
+++ Client/sketch.js
@@ -1,7 +1,72 @@
+// Connections
+let peer;
+let myId;
+let connection;
+let players = {};
+
+// Player setup
+let myPlayer;
+
+// Connection setup
+(function connect() {
+  // Create a Peer instance
+  peer = new Peer();
+
+  // Wait until the Peer is ready before allowing connections
+  peer.on("open", (id) => {
+    console.log("Client ID: " + id);
+    myId = id;
+
+    // Prompt for the Host's Peer ID to connect
+    let hostId = prompt("Enter Host Peer ID:");
+    if (hostId) {
+      connection = peer.connect(hostId);
+
+      connection.on("open", () => {
+        console.log("Connected to host: " + hostId);
+        connection.send("NEED DATA");
+      });
+
+      connection.on("data", (data) => {
+        console.log(data);
+        if (data === "FULL") {
+          peer.destroy();
+          return alert("Host server is full.\nRefresh the page to try again.");
+        }
+        if (data === "DIED") {
+          peer.destroy();
+          return alert("You died.\nRefresh the page to try again.\nHost ID: " + hostId);
+        }
+        if (players && myPlayer) {
+          
+          // Receive host's updates
+          players = data.players;
+          delete players[myId];
+        } else {
+          
+          // No information (first gather)
+          players = data.players;
+          myPlayer = data.players[myId];
+          delete players[myId];
+        }
+      });
+
+      connection.on("close", () => {
+        alert(`Disconnected from Host`);
+        peer.destroy();
+        players = {};
+        myPlayer = undefined;
+      });
+    } else peer.destroy();
+  });
+})();
+
 function setup() {
   createCanvas(480, 480);
 }
 
 function draw() {
+  if (!connection) return;
+
   background(50);
 }
--- Host/sketch.js
+++ Host/sketch.js
@@ -1,7 +1,90 @@
+// Connections
+let peer;
+let myId;
+let connections = {};
+let players = {};
+let active = false;
+
+// Player setup
+let myPlayer;
+
+// Connection setup
+(function connect() {
+  // Create a Peer instance
+  peer = new Peer();
+
+  // Log the ID assigned to this peer
+  peer.on("open", (id) => {
+    alert("My Host Peer ID: " + id);
+    console.log("Host ID: " + id);
+    myId = id;
+    myPlayer = {
+      id: id
+    };
+    active = true;
+  });
+
+  // Handle incoming connections
+  peer.on("connection", (connection) => {
+    let connected = true;
+    connections[connection.peer] = connection;
+    console.log("New Client ID: " + connection.peer);
+    players[connection.peer] = {
+      id: connection.peer
+    };
+
+    updateData();
+
+    connection.on("open", () => {
+      if (connected) {
+        updateData();
+      } else {
+        console.log("Server is full. Disconnecting new client...");
+        connection.send("FULL");
+        delete connections[connection.peer];
+        connection.close();
+      }
+    });
+
+    connection.on("data", (data) => {
+      if (data === "NEED DATA") {
+        updateData();
+      } else {
+        players[connection.peer] = data.player;
+        updateData();
+      }
+    });
+
+    connection.on("close", () => {
+      console.log(`Client ${connection.peer} disconnected`);
+      delete connections[connection.peer]; // Remove from active connections
+      delete players[connection.peer];
+      updateData();
+    });
+  });
+})();
+
 function setup() {
   createCanvas(480, 480);
 }
 
 function draw() {
+  if (!active) return;
+
   background(50);
+}
+
+// Host Functions
+
+function updateData() {
+  for (const connection in connections) {
+    if (connections[connection].open) {
+      connections[connection].send({
+        players: {
+          ...players,
+          [myId]: myPlayer,
+        }
+      });
+    }
+  }
 }
